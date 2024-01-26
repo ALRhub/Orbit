5574b5c5f6c81236972067e4e674d4fbc266d3cf
Jan Oberst <uyvch@student.kit.edu>
2024-01-26 11:14:44 +0100

    re-arranges files and imports fancy gym

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/config/franka/__init__.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/config/franka/__init__.py
@@ -5,6 +5,7 @@
 
 
 import gymnasium as gym
+import fancy_gym.envs.registry as fancy_gym_registry
 
 from . import agents, ik_abs_env_cfg, ik_rel_env_cfg, joint_pos_env_cfg
 
@@ -17,7 +18,7 @@ from . import agents, ik_abs_env_cfg, ik_rel_env_cfg, joint_pos_env_cfg
 ##
 
 gym.register(
-    id="Isaac-Push-Box-Franka-v0",
+    id="Isaac-Box-Pushing-Franka-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
     kwargs={
         "env_cfg_entry_point": joint_pos_env_cfg.FrankaBoxPushingEnvCfg,
@@ -27,7 +28,7 @@ gym.register(
 )
 
 gym.register(
-    id="Isaac-Push-Box-Franka-Play-v0",
+    id="Isaac-Box-Pushing-Franka-Play-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
     kwargs={
         "env_cfg_entry_point": joint_pos_env_cfg.FrankaBoxPushingEnvCfg_PLAY,
@@ -36,12 +37,17 @@ gym.register(
     disable_env_checker=True,
 )
 
+fancy_gym_registry.upgrade(
+    id="Isaac-Box-Pushing-Franka-v0",
+    mp_wrapper=joint_pos_env_cfg.BlackBoxPushBoxFrankaWrapper,
+)
+
 ##
 # Inverse Kinematics - Absolute Pose Control
 ##
 
 gym.register(
-    id="Isaac-Push-Box-Franka-IK-Abs-v0",
+    id="Isaac-Box-Pushing-Franka-IK-Abs-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
     kwargs={
         "env_cfg_entry_point": ik_abs_env_cfg.FrankaBoxPushingEnvCfg,
@@ -51,7 +57,7 @@ gym.register(
 )
 
 gym.register(
-    id="Isaac-Push-Box-Franka-IK-Abs-Play-v0",
+    id="Isaac-Box-Pushing-Franka-IK-Abs-Play-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
     kwargs={
         "env_cfg_entry_point": ik_abs_env_cfg.FrankaBoxPushingEnvCfg_PLAY,
@@ -65,7 +71,7 @@ gym.register(
 ##
 
 gym.register(
-    id="Isaac-Push-Box-Franka-IK-Rel-v0",
+    id="Isaac-Box-Pushing-Franka-IK-Rel-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
     kwargs={
         "env_cfg_entry_point": ik_rel_env_cfg.FrankaBoxPushingEnvCfg,
@@ -75,7 +81,7 @@ gym.register(
 )
 
 gym.register(
-    id="Isaac-Push-Box-Franka-IK-Rel-Play-v0",
+    id="Isaac-Box-Pushing-Franka-IK-Rel-Play-v0",
     entry_point="omni.isaac.orbit.envs:RLTaskEnv",
     kwargs={
         "env_cfg_entry_point": ik_rel_env_cfg.FrankaBoxPushingEnvCfg_PLAY,
