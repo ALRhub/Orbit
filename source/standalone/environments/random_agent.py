465d901aa1acfe89b6fb6281ab22cee035e0cfa6
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-02-13 13:05:01 +0100

    Added trajectory plotting

--- source/standalone/environments/random_agent.py
+++ source/standalone/environments/random_agent.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023, The ORBIT Project Developers.
+# Copyright (c) 2022-2024, The ORBIT Project Developers.
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -16,9 +16,22 @@ from omni.isaac.orbit.app import AppLauncher
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Random agent for Orbit environments.")
-parser.add_argument("--cpu", action="store_true", default=False, help="Use CPU pipeline.")
-parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
+parser.add_argument(
+    "--cpu", action="store_true", default=False, help="Use CPU pipeline."
+)
+parser.add_argument(
+    "--num_envs", type=int, default=None, help="Number of environments to simulate."
+)
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+parser.add_argument(
+    "--motion_primitive",
+    type=str,
+    default=None,
+    help=(
+        "Wether to use a motion primitive for the training. The supported ones depend in the environment: ProDMP,"
+        " etc..."
+    ),
+)
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -32,6 +45,7 @@ simulation_app = app_launcher.app
 
 
 import gymnasium as gym
+import matplotlib.pyplot as plt
 import torch
 import traceback
 
@@ -45,9 +59,14 @@ from omni.isaac.orbit_tasks.utils import parse_env_cfg
 def main():
     """Random actions agent with Orbit environment."""
     # parse configuration
-    env_cfg = parse_env_cfg(args_cli.task, use_gpu=not args_cli.cpu, num_envs=args_cli.num_envs)
+    env_cfg = parse_env_cfg(
+        args_cli.task, use_gpu=not args_cli.cpu, num_envs=args_cli.num_envs
+    )
     # create environment
-    env = gym.make(args_cli.task, cfg=env_cfg)
+    task_name = args_cli.task
+    if args_cli.motion_primitive is not None:
+        task_name = "gym_" + args_cli.motion_primitive + "/" + task_name
+    env = gym.make(task_name, cfg=env_cfg)
 
     # print info (this is vectorized environment)
     print(f"[INFO]: Gym observation space: {env.observation_space}")
@@ -55,16 +74,45 @@ def main():
     # reset environment
     env.reset()
     # simulate environment
+    rewards = []
+    infos = []
     while simulation_app.is_running():
         # run everything in inference mode
         with torch.inference_mode():
             # sample actions from -1 to 1
-            actions = 2 * torch.rand(env.action_space.shape, device=env.unwrapped.device) - 1
+            actions = (
+                2
+                * torch.rand(
+                    (args_cli.num_envs, env.action_space.shape[0]),
+                    device=env.unwrapped.device,
+                )
+                - 1
+            )
+            # actions = 2 * torch.rand(env.action_space.shape, device=env.unwrapped.device) - 1
             # apply actions
-            env.step(actions)
-
+            _, reward, _, _, info = env.step(actions)
+            rewards.append(reward[0].cpu().numpy())
+            infos.append(info)
     # close the simulator
+    # plot_trajectories(rewards, infos)
     env.close()
+
+
+def plot_trajectories(rewards, infos):
+    _, axs = plt.subplots(2, 4)
+    for i in range(len(rewards)):
+        position = infos[i]["positions"][0].transpose(0, 1).cpu().tolist()
+        axs[0, 0].plot(position[0], "b")
+        axs[0, 1].plot(position[1], "b")
+        axs[0, 2].plot(position[2], "b")
+        axs[0, 3].plot(position[3], "b")
+        axs[1, 0].plot(position[4], "b")
+        axs[1, 1].plot(position[5], "b")
+        axs[1, 2].plot(position[6], "b")
+        if i % 10 == 0:
+            print("Steps: ", i)
+    axs[1, 3].plot(rewards)
+    plt.show()
 
 
 if __name__ == "__main__":
