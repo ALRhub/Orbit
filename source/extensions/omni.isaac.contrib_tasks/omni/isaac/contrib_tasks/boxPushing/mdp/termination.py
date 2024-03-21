64c8f9694ef3ddfa94918785e5f0ede587e480ef
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-03-21 16:58:41 +0100

    Set success condition check only for the end of the episode

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/mdp/termination.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/mdp/termination.py
@@ -10,7 +10,7 @@ from typing import TYPE_CHECKING
 
 from omni.isaac.orbit.managers import SceneEntityCfg
 
-from .rewards import object_goal_position_distance
+from .rewards import object_goal_position_distance, object_goal_orientation_distance
 
 if TYPE_CHECKING:
     from omni.isaac.orbit.envs import RLTaskEnv
@@ -19,8 +19,11 @@ if TYPE_CHECKING:
 def is_success(
     env: RLTaskEnv,
     command_name: str,
-    limit: float = 0.05,
+    limit_pose_dist: float = 0.05,
+    limit_or_dist: float = 0.5,
     robot_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
     object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
 ) -> torch.Tensor:
-    return object_goal_position_distance(env, command_name, False, 0.0, robot_cfg, object_cfg) < limit
+    terminated = env.termination_manager.dones
+    return torch.where(terminated, torch.logical_and(object_goal_position_distance(env, command_name, False, 0.0, robot_cfg, object_cfg) < limit_pose_dist,
+                       object_goal_orientation_distance(env, command_name, robot_cfg, object_cfg) < limit_or_dist), False)
