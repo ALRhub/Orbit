cc560a7d3e3eec4283db9f15b05d96e410a9c549
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-05-01 12:38:28 +0200

    Removed unused IK envs

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/config/franka/__init__.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/config/franka/__init__.py
@@ -8,7 +8,7 @@ import gymnasium as gym
 
 import fancy_gym.envs.registry as fancy_gym_registry
 
-from . import agents, ik_abs_env_cfg, ik_rel_env_cfg, joint_pos_env_cfg
+from . import agents, joint_pos_env_cfg
 
 ##
 # Register Gym environments.
@@ -33,52 +33,4 @@ for reward_type in ["Dense", "TemporalSparse"]: #TODO add TemporalSpatialSparse
 fancy_gym_registry.upgrade(
     id="Isaac-Box-Pushing-Franka-v0",
     mp_wrapper=joint_pos_env_cfg.FrankaBoxPushingMPWrapper,
-)
-
-##
-# Inverse Kinematics - Absolute Pose Control
-##
-
-gym.register(
-    id="Isaac-Box-Pushing-Franka-IK-Abs-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    kwargs={
-        "env_cfg_entry_point": ik_abs_env_cfg.FrankaBoxPushingEnvCfg,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.BoxPushingPPORunnerCfg,
-    },
-    disable_env_checker=True,
-)
-
-gym.register(
-    id="Isaac-Box-Pushing-Franka-IK-Abs-Play-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    kwargs={
-        "env_cfg_entry_point": ik_abs_env_cfg.FrankaBoxPushingEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.BoxPushingPPORunnerCfg,
-    },
-    disable_env_checker=True,
-)
-
-##
-# Inverse Kinematics - Relative Pose Control
-##
-
-gym.register(
-    id="Isaac-Box-Pushing-Franka-IK-Rel-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    kwargs={
-        "env_cfg_entry_point": ik_rel_env_cfg.FrankaBoxPushingEnvCfg,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.BoxPushingPPORunnerCfg,
-    },
-    disable_env_checker=True,
-)
-
-gym.register(
-    id="Isaac-Box-Pushing-Franka-IK-Rel-Play-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    kwargs={
-        "env_cfg_entry_point": ik_rel_env_cfg.FrankaBoxPushingEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.BoxPushingPPORunnerCfg,
-    },
-    disable_env_checker=True,
 )
