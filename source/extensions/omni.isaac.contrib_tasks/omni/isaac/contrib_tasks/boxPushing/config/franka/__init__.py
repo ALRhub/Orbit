2d33ae8f10882119bdb095ca147d01e8d4fa04de
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-05-01 13:01:42 +0200

    Added selection of PPO runner cfg in env name

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/config/franka/__init__.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/boxPushing/config/franka/__init__.py
@@ -20,17 +20,18 @@ from . import agents, joint_pos_env_cfg
 
 # Dense reward
 for reward_type in ["Dense", "TemporalSparse"]: #TODO add TemporalSpatialSparse
-    gym.register(
-        id="Isaac-Box-Pushing-{}-Franka-v0".format(reward_type),
-        entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-        kwargs={
-            "env_cfg_entry_point": getattr(joint_pos_env_cfg, "FrankaBoxPushingEnvCfg_{}".format(reward_type)),
-            "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.BoxPushingPPORunnerCfg,
-        },
-        disable_env_checker=True,
-    )
+    for rsl_rl_cfg_name in ["Step_RL_Orbit_HP", "BBRL_Orbit_HP", "Step_RL_Fancy_Gym_HP", "BBRL_Fancy_Gym_HP"]:
+        gym.register(
+            id="Isaac-Box-Pushing-{reward}-{agent_cfg}-Franka-v0".format(reward=reward_type, agent_cfg=rsl_rl_cfg_name),
+            entry_point="omni.isaac.orbit.envs:RLTaskEnv",
+            kwargs={
+                "env_cfg_entry_point": getattr(joint_pos_env_cfg, "FrankaBoxPushingEnvCfg_{}".format(reward_type)),
+                "rsl_rl_cfg_entry_point": getattr(agents.rsl_rl_cfg, "BoxPushingPPORunnerCfg_{}".format(rsl_rl_cfg_name)),
+            },
+            disable_env_checker=True,
+        )
 
-fancy_gym_registry.upgrade(
-    id="Isaac-Box-Pushing-Franka-v0",
-    mp_wrapper=joint_pos_env_cfg.FrankaBoxPushingMPWrapper,
-)
+    fancy_gym_registry.upgrade(
+        id="Isaac-Box-Pushing-{reward}-{agent_cfg}-Franka-v0".format(reward=reward_type, agent_cfg=rsl_rl_cfg_name),
+        mp_wrapper=joint_pos_env_cfg.FrankaBoxPushingMPWrapper,
+    )
